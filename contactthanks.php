2cc33adef843be64c2bba267d5f7d1b65d973c38
TimmyDodd <timmydodd@gmail.com>
2016-06-27 12:50:46 -0400

    Create contactthanks.php
    success page

---
+++ contactthanks.php
@@ -0,0 +1,10 @@
+[header]
+	<div class="container">
+		<section id="mail" class="row">
+			<div class="col-xs-12">
+				<br>
+				<h1>Your message has been sent!</h1>
+			</div>
+		</section>
+	</div>
+[footer]
