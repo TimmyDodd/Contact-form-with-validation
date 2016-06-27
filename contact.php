f35b6f10deb214227016fd46924437541879f201
TimmyDodd <timmydodd@gmail.com>
2016-06-27 13:31:07 -0400

    Update contact.php
    formatting changes

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 	<form method="post" action="contactengine.php" name="contact-form" id="contact-form" onsubmit="return validateForm()">
 		<label for="Name">Name:</label>
 		<input type="text" name="Name" id="Name"/>
-		<label for="City">Phone:</label>
+		<label for="Tel">Phone:</label>
 		<input type="text" name="Tel" id="Tel"/>
 		<label for="Email">Email:</label>
 		<input type="text" name="Email" id="Email"/>
@@ -16,11 +16,11 @@
 <!-- 
 <div id="contact-area">
 	<form name="contact-form" id="contact-form" method="post" action="contactengine.php">
-		<input type="text" name="Name" id="Name" placeholder="Your Name *" class="form-control">
-		<input type="text" name="Tel" id="Tel" class="form-control" placeholder="Your Phone Number *">
-		<input type="text" name="Email" id="Email" class="form-control" placeholder="Your Email *">
-		<textarea name="Message" rows="10" cols="20" id="Message" class="form-control" placeholder="Your Message *" ></textarea>
-		<button type="submit" name="submit" value="Submit" class="btn btn-lg">Send</button>
+		<input type="text" class="form-control" name="Name"    id="Name"    placeholder="Your Name *">
+		<input type="text" class="form-control" name="Tel"     id="Tel"     placeholder="Your Phone Number *">
+		<input type="text" class="form-control" name="Email"   id="Email"   placeholder="Your Email *">
+		<textarea          class="form-control" name="Message" id="Message" placeholder="Your Message *" rows="10" cols="20"></textarea>
+		<button type="submit" class="btn btn-lg" name="submit" value="Submit">Send</button>
 		<div style="clear: both;"></div>
 	</form>
 </div>
